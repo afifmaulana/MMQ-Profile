be0195e02d32d03785b967cee7b7e255aba4e397
Afif Maulana Iskandar <afifmaulanaiskandar@gmail.com>
2020-12-26 16:25:54 +0700

    Laravel 8 & TailwindCss

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>MMQ - Majelis Murottilil Qur'an</title>
+</head>
+<body>
+
+    <x-navbar>
+
+    </x-navbar>
+
+    <div class="relativebg-white antialiased overflow-hidden">
+        <div class="max-w-7xl mx-auto">
+            <div class="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+                <x-main>
+
+                </x-main>
+            </div>
+        </div>
+        <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+            <img class="h-56 w-full object-cover sm:h-72 md:h-96 py-14 px-4 lg:w-full lg:h-full" src="images/banner.jpg" alt="">
+          </div>
+
+    </div>
+
+    <x-gallery></x-gallery>
+    <x-footer></x-footer>
+</body>
+</html>
